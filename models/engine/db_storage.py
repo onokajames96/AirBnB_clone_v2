0f09c6641271ac36be6ece427b842be2ff6a024a
onokajames96 <jamesonoka996@gmail.com>
2023-11-22 02:45:54 -0800

    database

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,3 +9,12 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+classes = {"Amenity": Amenity, "City": City,
+            "Place": Place, "Review": Review, "State": State, "User": User}
+
+class DBStorage:
