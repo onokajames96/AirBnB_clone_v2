941d8a9e537f6ed9521ba0c6940cfc5982be0200
onokajames96 <jamesonoka996@gmail.com>
2023-11-22 02:32:49 -0800

    db storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+import models
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
