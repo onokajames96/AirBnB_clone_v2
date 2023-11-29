e457a4791014fab0c827d44abe184c5cde843c11
onokajames96 <jamesonoka996@gmail.com>
2023-11-28 22:02:33 -0800

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,6 +66,3 @@ class DBStorage:
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
-
-
-
